8d775414ba3e827a62e77828cd6d63e12e3a69c1
sushin <shresthasushin@gmail.com>
2013-10-16 21:10:34 +0545

    first commit

---
+++ projectionSystem/tank.c
@@ -0,0 +1,36 @@
+#include <graphics.h>
+#include <stdio.h>
+#include <conio.h>
+
+
+//methods to initialize the graphics
+void initGraphics(){
+int gd=DETECT,gm;
+initgraph(&gd,&gm,"C:\\TC\\BGI");
+}
+
+//methods to obtain x and y co-ordinate
+int getProjectileX(float timeIndex, float theta, float velocity){
+ return ((velocity*cos(theta*_PI_/180))*timeIndex);
+}
+
+int getProjectileY(float timeIndex, float theta, float velocity){
+  return (velocity*sin(theta*_PI_/180))*timeIndex-0.5*_GRAVITY_*(timeIndex*timeIndex);
+}
+
+void startGame(){
+   while(!kbhit()){
+   
+   
+   }
+   
+}
+
+//program entry point
+void main(){
+   initGraphics();
+   
+
+
+
+}
